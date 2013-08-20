c02322e1f6c7fbf3ef5d5033241dcde6033307ec
Eric Ryan <ewryan@gmail.com>
2013-08-20 17:48:01 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fisherator::Application.config.secret_key_base = 'de920a3efe5f79e787e002eba30cda6c6d3c437495365a7d1da785742d02c02e4f99f7b8468c0250a3da04b12bca7f302f06c1e5b27c30b74bd0cae265843059'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fisherator::Application.config.session_store :cookie_store, key: '_fisherator_session'
